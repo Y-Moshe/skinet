2bf8450b7215eeaf8169a0683838253b867f58ef
Y-Moshe <moshe.nehemiah@gmail.com>
2023-03-15 01:14:10 +0200

    Product controller repository

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,3 +1,5 @@
+using Core.Entities;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,16 +8,24 @@ namespace API.Controllers
   [Route("api/[controller]")]
   public class ProductsController : ControllerBase
   {
-    [HttpGet]
-    public string getProducts()
+    private IProductRepository productsRepository { get; set; }
+    public ProductsController(IProductRepository productRepository)
     {
-      return "getting products";
+      this.productsRepository = productRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> getProducts()
+    {
+      var products = await this.productsRepository.GetProductsAsync();
+      return Ok(products);
     }
 
     [HttpGet("{id}")]
-    public string getProduct(int id)
+    public async Task<ActionResult<Product>> getProduct(int id)
     {
-      return "getting a single product";
+      var product = await this.productsRepository.GetProductByIdAsync(id);
+      return Ok(product);
     }
   }
 }
