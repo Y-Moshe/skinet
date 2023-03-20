a894e3dfc6c6ee531862cb656789b3a86538cf7a
Y-Moshe <moshe.nehemiah@gmail.com>
2023-03-20 03:54:15 +0200

    implement searching

--- Core/Specifications/ProductsWithFiltersForCountSpecification.cs
+++ Core/Specifications/ProductsWithFiltersForCountSpecification.cs
@@ -6,6 +6,7 @@ namespace Core.Specifications
   {
     public ProductsWithFiltersForCountSpecification(ProductsParamsSpecification productsParams)
     : base(p => (
+        (string.IsNullOrEmpty(productsParams.Search) || p.Name.ToLower().Contains(productsParams.Search)) &&
         (!productsParams.BrandId.HasValue || p.ProductBrandId == productsParams.BrandId) &&
         (!productsParams.TypeId.HasValue || p.ProductTypeId == productsParams.TypeId)
       ))
