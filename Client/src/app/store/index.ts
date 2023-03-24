54be750bf51955cc9aed1f8d5825457b1fd07138
Y-Moshe <moshe.nehemiah@gmail.com>
2023-03-24 08:07:53 +0300

    install dev-tools for ngrx

--- Client/src/app/store/index.ts
+++ Client/src/app/store/index.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core'
 import { EffectsModule } from '@ngrx/effects'
 import { StoreModule, ActionReducerMap } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { LetModule, PushModule } from '@ngrx/component'
+import { environment } from '../../environments/environment'
 
 import { AuthEffects } from './effects/auth'
 import authReducer, { IAuthState } from './reducers/auth'
@@ -26,6 +28,10 @@ const reducers: ActionReducerMap<IAppState> = {
   imports: [
     StoreModule.forRoot(reducers),
     EffectsModule.forRoot([AuthEffects, ShopEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
     LetModule,
     PushModule,
   ],
