19dc557b6158aa27ed93462a42eb68a09f3ea391
Y-Moshe <moshe.nehemiah@gmail.com>
2023-03-27 03:19:37 +0300

    set all optional as they are

--- Client/src/app/types/shop-params.ts
+++ Client/src/app/types/shop-params.ts
@@ -1,8 +1,8 @@
 export interface IShopFilterByParams {
-  brandId: number
-  typeId: number
-  sort: string
-  pageNumber: number
-  pageSize: number
-  search: string
+  brandId?: number
+  typeId?: number
+  sort?: string
+  pageNumber?: number
+  pageSize?: number
+  search?: string
 }
