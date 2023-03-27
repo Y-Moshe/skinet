9342b45fd5086dbf1a779bd570abb0a8cb045eb3
Y-Moshe <moshe.nehemiah@gmail.com>
2023-03-27 08:26:57 +0300

    add LetModule

--- Client/src/app/features/shop/shop.module.ts
+++ Client/src/app/features/shop/shop.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common'
-import { PushModule } from '@ngrx/component'
+import { LetModule, PushModule } from '@ngrx/component'
 
 import { ShopComponent } from './shop.component'
 import { ProductListComponent } from './product-list/product-list.component'
 import { ProductPreviewComponent } from './product-list/product-preview/product-preview.component'
 import { ProductDetailsComponent } from './product-details/product-details.component'
 import { ProductFiltersComponent } from './product-filters/product-filters.component'
+
 import { ShopRoutingModule } from './shop-routing.module'
-import { AppPrimeNGModule } from '@/app-primeng.module';
-import { ProductSearchComponent } from './product-search/product-search.component';
+import { ProductSearchComponent } from './product-search/product-search.component'
 import { ProductSortComponent } from './product-sort/product-sort.component'
+import { AppPrimeNGModule } from '@/app-primeng.module'
 
 @NgModule({
   declarations: [
@@ -23,6 +24,13 @@ import { ProductSortComponent } from './product-sort/product-sort.component'
     ProductSearchComponent,
     ProductSortComponent,
   ],
-  imports: [CommonModule, AppPrimeNGModule, ShopRoutingModule, PushModule, FormsModule],
+  imports: [
+    CommonModule,
+    AppPrimeNGModule,
+    ShopRoutingModule,
+    PushModule,
+    LetModule,
+    FormsModule,
+  ],
 })
 export class ShopModule {}
