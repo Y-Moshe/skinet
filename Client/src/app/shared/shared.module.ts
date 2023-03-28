caef0c644f1a2a761ca492034e7cda93c921a1c3
Y-Moshe <moshe.nehemiah@gmail.com>
2023-03-28 08:42:22 +0300

    BlockableDiv component, used with BlockUI

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common'
 import { ReactiveFormsModule } from '@angular/forms'
 
 import { CommonInputComponent } from './common-input/common-input.component'
+import { BlockableDiv } from './blockable-div/blockable-div.component'
 import { AppPrimeNGModule } from '@/app-primeng.module'
 
 @NgModule({
-  declarations: [CommonInputComponent],
+  declarations: [CommonInputComponent, BlockableDiv],
   imports: [CommonModule, AppPrimeNGModule, ReactiveFormsModule],
-  exports: [CommonInputComponent],
+  exports: [CommonInputComponent, BlockableDiv],
 })
 export class SharedModule {}
