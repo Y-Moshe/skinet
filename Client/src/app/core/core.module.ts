770d0115f2f4d486a42621294c048dd35302c906
Y-Moshe <moshe.nehemiah@gmail.com>
2023-03-30 15:35:47 +0300

    add subheader component for breadcrumbs

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PushModule } from '@ngrx/component';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { PushModule } from '@ngrx/component'
 
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { E404Component } from './e404/e404.component';
+import { HeaderComponent } from './header/header.component'
+import { FooterComponent } from './footer/footer.component'
+import { HomeComponent } from './home/home.component'
+import { E404Component } from './e404/e404.component'
 
-import { AppRoutingModule } from '../app-routing.module';
-import { AppPrimeNGModule } from '../app-primeng.module';
+import { AppRoutingModule } from '../app-routing.module'
+import { AppPrimeNGModule } from '../app-primeng.module'
+import { SubheaderComponent } from './subheader/subheader.component'
 
 @NgModule({
   declarations: [
@@ -16,8 +17,9 @@ import { AppPrimeNGModule } from '../app-primeng.module';
     FooterComponent,
     HomeComponent,
     E404Component,
+    SubheaderComponent,
   ],
   imports: [CommonModule, AppRoutingModule, AppPrimeNGModule, PushModule],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, SubheaderComponent],
 })
 export class CoreModule {}
