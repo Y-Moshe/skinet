ae4231c06e3ff76c5e78f3759d2feab8d7ff47c7
Y-Moshe <moshe.nehemiah@gmail.com>
2023-04-03 00:16:11 +0300

    move to shared module

--- Client/src/app/features/basket/basket.module.ts
+++ Client/src/app/features/basket/basket.module.ts
@@ -4,15 +4,12 @@ import { FormsModule } from '@angular/forms'
 import { LetModule, PushModule } from '@ngrx/component'
 
 import { BasketComponent } from './basket.component'
-import { BasketListComponent } from './basket-list/basket-list.component'
-import { BasketItemComponent } from './basket-item/basket-item.component'
-
 import { BasketRoutingModule } from './basket-routing.module'
 import { AppPrimeNGModule } from '@/app-primeng.module'
 import { SharedModule } from '@/shared/shared.module'
 
 @NgModule({
-  declarations: [BasketComponent, BasketListComponent, BasketItemComponent],
+  declarations: [BasketComponent],
   imports: [
     CommonModule,
     BasketRoutingModule,
--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -5,11 +5,25 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { CommonInputComponent } from './common-input/common-input.component'
 import { BlockableDiv } from './blockable-div/blockable-div.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component'
+import { BasketListComponent } from './basket-list/basket-list.component'
+import { BasketItemComponent } from './basket-list/basket-item/basket-item.component'
 import { AppPrimeNGModule } from '@/app-primeng.module'
 
 @NgModule({
-  declarations: [CommonInputComponent, BlockableDiv, OrderSummaryComponent],
+  declarations: [
+    CommonInputComponent,
+    BlockableDiv,
+    OrderSummaryComponent,
+    BasketListComponent,
+    BasketItemComponent,
+  ],
   imports: [CommonModule, AppPrimeNGModule, ReactiveFormsModule],
-  exports: [CommonInputComponent, BlockableDiv, OrderSummaryComponent],
+  exports: [
+    CommonInputComponent,
+    BlockableDiv,
+    OrderSummaryComponent,
+    BasketListComponent,
+    BasketItemComponent,
+  ],
 })
 export class SharedModule {}
