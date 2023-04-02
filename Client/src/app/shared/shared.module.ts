affe639d32af98a3158e4fe1225f9346e8bb92db
Y-Moshe <moshe.nehemiah@gmail.com>
2023-04-02 08:08:53 +0300

    add shared OrderSummaryComponent

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms'
 
 import { CommonInputComponent } from './common-input/common-input.component'
 import { BlockableDiv } from './blockable-div/blockable-div.component'
+import { OrderSummaryComponent } from './order-summary/order-summary.component'
 import { AppPrimeNGModule } from '@/app-primeng.module'
 
 @NgModule({
-  declarations: [CommonInputComponent, BlockableDiv],
+  declarations: [CommonInputComponent, BlockableDiv, OrderSummaryComponent],
   imports: [CommonModule, AppPrimeNGModule, ReactiveFormsModule],
-  exports: [CommonInputComponent, BlockableDiv],
+  exports: [CommonInputComponent, BlockableDiv, OrderSummaryComponent],
 })
 export class SharedModule {}
