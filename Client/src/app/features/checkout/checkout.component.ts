5d6b2cb0ca872b4348a3a3f026420490ed4b3a15
Y-Moshe <moshe.nehemiah@gmail.com>
2023-04-05 12:09:24 +0300

    checkout layout, order summary

--- Client/src/app/features/checkout/checkout.component.ts
+++ Client/src/app/features/checkout/checkout.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
+import { Store } from '@ngrx/store'
+import { IAppState, selectors } from '@/store'
 import { MenuItem } from 'primeng/api'
+import { IBasketState } from '@/store/reducers/basket'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit, OnDestroy {
   activeIndex = 0
   checkoutSteps: MenuItem[] = [
     {
@@ -27,7 +31,16 @@ export class CheckoutComponent {
     },
   ]
 
-  constructor(private router: Router) {}
+  bState!: IBasketState
+  bStateSub!: Subscription
+
+  constructor(private router: Router, private store$: Store<IAppState>) {}
+
+  ngOnInit(): void {
+    this.bStateSub = this.store$
+      .select(selectors.selectBasketState)
+      .subscribe((state) => (this.bState = state))
+  }
 
   handleStepsClick(isNext: boolean) {
     const stepIdx = isNext ? this.activeIndex + 1 : this.activeIndex - 1
@@ -37,5 +50,13 @@ export class CheckoutComponent {
     const nextRoute = activeStep.routerLink
     this.router.navigate(['/checkout/' + nextRoute], { replaceUrl: true })
     this.activeIndex += isNext ? 1 : -1
+  }
+
+  handlePlaceOrder() {
+    console.log('Ordered')
+  }
+
+  ngOnDestroy(): void {
+    this.bStateSub.unsubscribe()
   }
 }
