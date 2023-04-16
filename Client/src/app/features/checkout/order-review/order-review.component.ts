d26d442c802160101d22432f095d7e913878c2bf
Y-Moshe <moshe.nehemiah@gmail.com>
2023-04-17 00:32:12 +0300

    creating PaymentIntent on order review

--- Client/src/app/features/checkout/order-review/order-review.component.ts
+++ Client/src/app/features/checkout/order-review/order-review.component.ts
@@ -1,8 +1,9 @@
-import { IAppState, selectors } from '@/store'
 import { Component, OnInit, inject } from '@angular/core'
-import { Observable } from 'rxjs'
-
 import { Store } from '@ngrx/store'
+import { Observable, lastValueFrom, take } from 'rxjs'
+
+import { IAppState, actions, selectors } from '@/store'
+import { NotificationService, PaymentService } from '@/services'
 import { IBasket } from '@/types'
 
 @Component({
@@ -11,9 +12,27 @@ import { IBasket } from '@/types'
 })
 export class OrderReviewComponent implements OnInit {
   private readonly store$ = inject(Store<IAppState>)
+  private readonly paymentService = inject(PaymentService)
+  private readonly notificationService = inject(NotificationService)
+
   basket$!: Observable<IBasket>
 
   ngOnInit(): void {
     this.basket$ = this.store$.select(selectors.selectBasket)
+    this.createPaymentIntent()
+  }
+
+  async createPaymentIntent() {
+    const basket = await lastValueFrom(this.basket$.pipe(take(1)))
+
+    this.paymentService.createPaymentIntent(basket.id!).subscribe({
+      next: (basket) => this.store$.dispatch(actions.updateBasket({ basket })),
+      error: (err) =>
+        this.notificationService.notifyAtBottomMiddle({
+          summary: 'Payment API',
+          detail: err.message,
+          severity: 'error',
+        }),
+    })
   }
 }
