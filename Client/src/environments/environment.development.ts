b4d017e2a188daf257e96710ff04bed632cfccdb
Y-Moshe <moshe.nehemiah@gmail.com>
2023-04-27 22:26:37 +0300

    disable cache for development in client

--- Client/src/environments/environment.development.ts
+++ Client/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api',
   maxLoginAttempts: 5,
-  useCache: true,
+  useCache: false,
   isEditMode: true,
   stripePublisableKey: process.env.NG_APP_STRIPE_PUBLISABLE_KEY,
 }
