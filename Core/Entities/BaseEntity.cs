2bc7b40b1e01f95ad9f0f28a9bffc07dc10c75bd
Y-Moshe <moshe.nehemiah@gmail.com>
2023-06-30 16:47:52 +0300

    Remove the nullable props

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -2,6 +2,6 @@ namespace Core.Entities;
 
 public class BaseEntity
 {
-    public int? Id { get; set; }
-    public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
